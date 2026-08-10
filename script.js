e22b6074146824a1e61fb275dfc32e55b22fd0d0
Adryan micael de oliveira <adrian.micael.oliveira@escola.pr.gov.br>
2026-08-10 09:57:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//VERSÂO 1
+function funcao1(){    
+   var x = document.getElementById("i1").value
+   var y = document.getElementById("i2").value
+   var z = x * y;
+
+   if (z > 100) { 
+    document,getElementByd("res").style.color = "red";
+   alert("muito caro!");
+   } else {   
+       document.getElementByd("res").style.color = "red";
+
+}
+  document.getElementByd("res").innerHTML - "total: " + z;
+  console.log("calculado");
+
+
+
+}
